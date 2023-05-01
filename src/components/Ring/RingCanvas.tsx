a3d1f4464a566697d63fe4665e882e4bbe11f438
Anoop Jadhav <anoop.jadhav@thoughtworks.com>
2023-05-01 22:15:33 +0530

    improve model details

--- src/components/Ring/RingCanvas.tsx
+++ src/components/Ring/RingCanvas.tsx
@@ -1,12 +1,11 @@
-import { PresentationControls } from "@react-three/drei";
+import { PresentationControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Leva, useControls } from "leva";
-import { ReactNode, Suspense, useRef } from "react";
-import { Color, DirectionalLight } from "three";
-import Loader3D from "../Loader3D/Loader3D";
+import { ReactNode, Suspense } from "react";
+import { LoadingCube } from "../Loader3D/LoadingCube";
 import { ConfigContextProvider } from "./ConfigContext";
-import { Model } from "./Model";
 import ConfigInterface from "./ConfigInterface";
+import { Model } from "./Model";
 
 const RingCanvasWrapper = ({ children }: { children: ReactNode }) => {
   return (
@@ -24,8 +23,11 @@ const RingCanvasWrapper = ({ children }: { children: ReactNode }) => {
         frameloop="always"
         dpr={window?.devicePixelRatio}
         flat
-        camera={{ fov: 25, position: [0, 0, 25] }}
         shadows
+        camera={{ fov: 25, position: [0, 0, 25] }}
+        onCreated={(state) => {
+          state.gl.setClearColor("#fff1f4");
+        }}
       >
         {children}
       </Canvas>
@@ -35,55 +37,24 @@ const RingCanvasWrapper = ({ children }: { children: ReactNode }) => {
 };
 
 export function RingCanvas() {
-  const canvasColor = useControls("Canvas Color", {
-    color: "#fff1f4",
-  });
-
-  const ambientLight = useControls("AmbientLight", {
-    intensity: 1,
-  });
-
-  const areaLight = useControls("Area Light", {
-    color: "red",
-    width: 500,
-    height: 500,
-    position: [0, 0, 5],
-    intensity: 1,
-  });
-
-  const directionalLight = useControls("Directional Light", {
-    color: "red",
-    intensity: 1,
-  });
-
-  const directionLightRef = useRef<DirectionalLight>(null);
-
   return (
     <>
       <Leva flat collapsed />
       <RingCanvasWrapper>
-        <color attach="background" args={[canvasColor.color]} />
-        <PresentationControls
-          global
-          zoom={0.8}
-          rotation={[0, -Math.PI / 2, -Math.PI / 5]}
-          snap
-        >
-          <ambientLight intensity={ambientLight.intensity} />
-          <directionalLight {...directionalLight} ref={directionLightRef} />
-          <rectAreaLight
-            width={areaLight.width}
-            height={areaLight.height}
-            position={areaLight.position}
-            intensity={areaLight.intensity}
-            color={new Color(areaLight.color)}
-          />
-          <Suspense fallback={<Loader3D />}>
-            <Model />
+        <Stage environment="park" shadows={true} adjustCamera={false}>
+          <PresentationControls
+            global
+            zoom={0.8}
+            rotation={[0, -Math.PI / 2, -Math.PI / 5]}
+            snap
+          >
+            <Suspense fallback={<LoadingCube />}>
+              <Model />
+            </Suspense>
             {/* <axesHelper args={[5]} /> */}
             {/* <OrbitControls /> */}
-          </Suspense>
-        </PresentationControls>
+          </PresentationControls>
+        </Stage>
       </RingCanvasWrapper>
     </>
   );
